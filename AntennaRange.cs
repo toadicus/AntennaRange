78a70f90d7a1156f3f7302c00b220a676d2439ff
Andy Wilkinson <andy@wtribe.com>
2013-11-02 11:22:30 -0700

    Added MuMech_ToSI to pretty up the ranges when we report them to users.

--- AntennaRange.cs
+++ AntennaRange.cs
@@ -13,6 +13,8 @@
  * 
  * This software uses the ModuleManager library © 2013 ialdabaoth, used under a Creative Commons Attribution-ShareAlike 3.0 Uported License.
  * 
+ * This software uses code from the MuMechLib library, © 2013 r4m0n, used under the GNU GPL version 3.
+ * 
  */
 
 using System;
@@ -187,12 +189,12 @@ namespace AntennaRange
 		{
 			string ErrorText = string.Format (
 				"Unable to transmit: out of range!  Maximum range = {0}; Current range = {1}.",
-				this.maxTransmitDistance,
-				this.transmitDistance);
+				Tools.MuMech_ToSI((double)this.maxTransmitDistance),
+				Tools.MuMech_ToSI((double)this.transmitDistance));
 			ScreenMessages.PostScreenMessage (new ScreenMessage (ErrorText, 4f, ScreenMessageStyle.UPPER_LEFT));
 		}
 
-		// Before transmission, set packetResourceCost.  Per above, packet size increases with the square of
+		// Before transmission, set packetResourceCost.  Per above, packet cost increases with the square of
 		// distance.  packetResourceCost maxes out at _basepacketResourceCost * maxPowerFactor, at which point
 		// transmission fails (see CanTransmit).
 		protected void PreTransmit_SetPacketResourceCost()
@@ -228,8 +230,8 @@ namespace AntennaRange
 		public override string GetInfo()
 		{
 			string text = base.GetInfo();
-			text += "Nominal Range: " + this.nominalRange.ToString() + "\n";
-			text += "Maximum Range: " + this.maxTransmitDistance.ToString() + "\n";
+			text += "Nominal Range: " + Tools.MuMech_ToSI((double)this.nominalRange) + "\n";
+			text += "Maximum Range: " + Tools.MuMech_ToSI((double)this.maxTransmitDistance) + "\n";
 			return text;
 		}
 
@@ -340,6 +342,96 @@ namespace AntennaRange
 			else
 			{
 				KSPLog.print(Msg);
+			}
+		}
+
+		/*
+		 * MuMech_ToSI is a part of the MuMechLib library, © 2013 r4m0n, used under the GNU GPL version 3.
+		 * */
+		public static string MuMech_ToSI(double d, int digits = 3, int MinMagnitude = 0, int MaxMagnitude = int.MaxValue)
+		{
+			float exponent = (float)Math.Log10(Math.Abs(d));
+			exponent = UnityEngine.Mathf.Clamp(exponent, (float)MinMagnitude, (float)MaxMagnitude);
+
+			if (exponent >= 0)
+			{
+				switch ((int)Math.Floor(exponent))
+				{
+					case 0:
+						case 1:
+						case 2:
+						return d.ToString("F" + digits);
+						case 3:
+						case 4:
+						case 5:
+						return (d / 1e3).ToString("F" + digits) + "k";
+						case 6:
+						case 7:
+						case 8:
+						return (d / 1e6).ToString("F" + digits) + "M";
+						case 9:
+						case 10:
+						case 11:
+						return (d / 1e9).ToString("F" + digits) + "G";
+						case 12:
+						case 13:
+						case 14:
+						return (d / 1e12).ToString("F" + digits) + "T";
+						case 15:
+						case 16:
+						case 17:
+						return (d / 1e15).ToString("F" + digits) + "P";
+						case 18:
+						case 19:
+						case 20:
+						return (d / 1e18).ToString("F" + digits) + "E";
+						case 21:
+						case 22:
+						case 23:
+						return (d / 1e21).ToString("F" + digits) + "Z";
+						default:
+						return (d / 1e24).ToString("F" + digits) + "Y";
+				}
+			}
+			else if (exponent < 0)
+			{
+				switch ((int)Math.Floor(exponent))
+				{
+					case -1:
+						case -2:
+						case -3:
+						return (d * 1e3).ToString("F" + digits) + "m";
+						case -4:
+						case -5:
+						case -6:
+						return (d * 1e6).ToString("F" + digits) + "μ";
+						case -7:
+						case -8:
+						case -9:
+						return (d * 1e9).ToString("F" + digits) + "n";
+						case -10:
+						case -11:
+						case -12:
+						return (d * 1e12).ToString("F" + digits) + "p";
+						case -13:
+						case -14:
+						case -15:
+						return (d * 1e15).ToString("F" + digits) + "f";
+						case -16:
+						case -17:
+						case -18:
+						return (d * 1e18).ToString("F" + digits) + "a";
+						case -19:
+						case -20:
+						case -21:
+						return (d * 1e21).ToString("F" + digits) + "z";
+						default:
+						return (d * 1e24).ToString("F" + digits) + "y";
+				}
+			}
+			else
+			{
+				return "0";
 			}
 		}
 	}
